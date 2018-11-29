5e1895f2cfac51c98444e35207276e844907523f
Ryan Bell <rtbell@wisc.edu>
2018-11-28 22:39:18 -0600

    Create Project

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Scene root = FXMLLoader.load(getClass().getResource("/FoodAnalyzer.fxml"));
+			primaryStage.setScene(root);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
