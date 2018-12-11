09bee0358f4ef5e467d5d5e1f21ea05bedd10e80
NicolasM42 <mewhirter@wisc.edu>
2018-12-10 23:01:46 -0600

    Major implementation changes to BPTree. ~Nick

--- src/application/BPTree.java
+++ src/application/BPTree.java
@@ -6,9 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Random;
-import BPTree.InternalNode;
-import BPTree.LeafNode;
-import BPTree.Node;
 import java.util.Collections;
 
 /**
